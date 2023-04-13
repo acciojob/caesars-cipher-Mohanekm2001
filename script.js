fa5da01c4586b03d1f73287ce58f7fd24056ff94
Mohanekm2001 <124660796+Mohanekm2001@users.noreply.github.com>
2023-04-13 11:30:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 // Your Script here.
+function rot13(str) {
+  let decoded = '';
+  
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
+    
+    // Check if the character is uppercase alphabetic
+    if (charCode >= 65 && charCode <= 90) {
+      charCode = ((charCode - 65 + 13) % 26) + 65;
+    }
+    
+    decoded += String.fromCharCode(charCode);
+  }
+  
+  return decoded;
+}
+
+const inputField = document.getElementById('input');
+const outputField = document.getElementById('output');
+const encodeButton = document.getElementById('encode-button');
+const decodeButton = document.getElementById('decode-button');
+
+encodeButton.addEventListener('click', () => {
+  const message = inputField.value;
+  const encodedMessage = rot13(message);
+  outputField.value = encodedMessage;
+});
+
+decodeButton.addEventListener('click', () => {
+  const message = inputField.value;
+  const decodedMessage = rot13(message);
+  outputField.value = decodedMessage;
+});
+
 
 const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
